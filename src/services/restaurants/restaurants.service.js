560a0633d699587cf7810229268570eec82fa255
CMEI Tester2 <cmeitester2@gmail.com>
2022-01-23 20:37:50 -0600

    Section-153 completed. Installed camelize function

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
     return new Promise((resolve, reject) => {
@@ -10,12 +11,17 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
     });
 };
 
-// restaurantRequest().then((result) => {
-//     console.log(result);
-// });
+const restaurantsTransform = (result) => {
+    const newResult = camelize(result);
+    return newResult;
+};
 
-restaurantRequest().then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log('error');
-})
+restaurantRequest()
+    .then(restaurantsTransform)
+    .then(transformedResponse => {
+        console.log(transformedResponse);
+    })
+    .catch((err) => {
+        console.log('error');
+    }
+)
